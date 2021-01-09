9aefb2311a2be9c0ede45aef5299a67c14cd8f12
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-01-09 15:48:48 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,9 @@
 
 #sudo apt update -y && sudo apt upgrade -y
 #sudo raspi-config
-sudo apt install -y xorg xinit spectrwm cool-retro-term ranger mediainfo mpv nfs-common xscreensaver
+echo 'deb http://deb.xanmod.org releases main' | tee /etc/apt/sources.list.d/xanmod-kernel.list && wget -qO - https://dl.xanmod.org/gpg.key | apt-key add -
+sudo apt update -y
+sudo apt install -y linux-xanmod-edge xorg xinit spectrwm cool-retro-term ranger mediainfo mpv nfs-common xscreensaver
 echo 'if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then' > "$(ls -a /home/pi | grep profile)"
 echo '  exec startx' >> "$(ls -a /home/pi | grep profile)"
 echo 'fi' >> "$(ls -a /home/pi | grep profile)"
