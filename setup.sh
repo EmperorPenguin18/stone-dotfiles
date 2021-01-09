8d5391abf4210073cf598f99a2605d82292573b2
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-01-09 14:29:24 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-sudo apt update -y && sudo apt upgrade -y
-sudo raspi-config
-sudo apt install -y xorg xinit spectrwm cool-retro-term ranger mediainfo mpv nfs-common
+#sudo apt update -y && sudo apt upgrade -y
+#sudo raspi-config
+sudo apt install -y xorg xinit spectrwm cool-retro-term ranger mediainfo mpv nfs-common xscreensaver
+echo 'if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then' > "$(ls -a /home/pi | grep profile)"
+echo '  exec startx' >> "$(ls -a /home/pi | grep profile)"
+echo 'fi' >> "$(ls -a /home/pi | grep profile)"
 cp -f .xinitrc /home/pi/
 cp -f .spectrwm.conf /home/pi/
 mkdir -p /home/pi/.config/mpv
 cp -f mpv.conf /home/pi/.config/mpv/
+sudo cp -f mediamount.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable mediamount
