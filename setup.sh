3d84070f774d6d2175b737e2715469eb1478c27b
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-01-09 17:14:27 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,14 +5,13 @@
 echo 'deb http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-kernel.list && wget -qO - https://dl.xanmod.org/gpg.key | sudo apt-key add -
 sudo apt update -y
 sudo apt install -y linux-xanmod-edge xorg xinit spectrwm cool-retro-term ranger mediainfo mpv nfs-common xscreensaver
-echo 'if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then' > "$(ls -a /home/pi | grep profile)"
-echo '  exec startx' >> "$(ls -a /home/pi | grep profile)"
-echo 'fi' >> "$(ls -a /home/pi | grep profile)"
+echo 'if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then' > /home/pi/"$(ls -a /home/pi | grep profile)"
+echo '  exec startx' >> /home/pi/"$(ls -a /home/pi | grep profile)"
+echo 'fi' >> /home/pi/"$(ls -a /home/pi | grep profile)"
 cp -f .xinitrc /home/pi/
 cp -f .spectrwm.conf /home/pi/
 mkdir -p /home/pi/.config/mpv
 cp -f mpv.conf /home/pi/.config/mpv/
-sudo chown -R pi:pi /mnt
 sudo cp -f mediamount.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable mediamount
