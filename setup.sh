151a46ef00175ff7be0c0dbbc23addb6be9c3393
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-01-09 18:39:11 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 #sudo apt update -y && sudo apt upgrade -y
 #sudo raspi-config
 sudo apt install -y xorg xinit spectrwm ranger mediainfo mpv nfs-common xscreensaver
-echo 'if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then' > /home/pi/"$(ls -a /home/pi | grep profile)"
+echo 'if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then' >> /home/pi/"$(ls -a /home/pi | grep profile)"
 echo '  exec startx' >> /home/pi/"$(ls -a /home/pi | grep profile)"
 echo 'fi' >> /home/pi/"$(ls -a /home/pi | grep profile)"
 cp -f .xinitrc /home/pi/
