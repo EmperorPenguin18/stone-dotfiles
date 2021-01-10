972a53271d123df17c034c2584856124040f871b
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-01-09 20:53:31 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 #sudo apt update -y && sudo apt upgrade -y
 #sudo raspi-config
-sudo apt install -y xorg xinit spectrwm ranger mediainfo mpv nfs-common xscreensaver unclutter snap
+sudo apt install -y xorg xinit spectrwm ranger mediainfo mpv nfs-common xscreensaver unclutter snap xserver-xorg-input-joystick
 sudo snap install --classic cool-retro-term
 echo 'if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then' >> /home/pi/"$(ls -a /home/pi | grep profile)"
 echo '  exec startx' >> /home/pi/"$(ls -a /home/pi | grep profile)"
@@ -12,3 +12,4 @@ cp -f .spectrwm.conf /home/pi/
 mkdir -p /home/pi/.config/mpv
 cp -f mpv.conf /home/pi/.config/mpv/
 sudo echo '10.0.0.47:/mnt/MergerFS /mnt nfs rw' >> /etc/fstab
+sudo cp -f 51-joystick.conf /etc/X11/xorg.conf.d/
