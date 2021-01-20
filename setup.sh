89d5f9d93e410563b20bd57556cd724a8129c11f
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-01-19 20:22:31 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
+#Make sure to be all updated and configured before starting
 
-#sudo pacman -Syu
-#sudo raspi-config
-sudo pacman -S git base-devel xorg xorg-drivers xorg-xinit spectrwm termite ranger mpv nfs-utils xscreensaver unclutter --noconfirm --needed
-git clone https://aur.archlinux.org/pikaur.git
-cd pikaur
-makepkg -si --noconfirm
-pikaur -S xf86-input-joystick --noconfirm
-echo 'if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then' >> /home/alarm/"$(ls -a /home/alarm | grep profile)"
-echo '  exec startx' >> /home/alarm/"$(ls -a /home/alarm | grep profile)"
-echo 'fi' >> /home/alarm/"$(ls -a /home/alarm | grep profile)"
-cp -f ./.xinitrc /home/alarm/
-cp -f ./.spectrwm.conf /home/alarm/
-mkdir -p /home/alarm/.config/mpv
-cp -f ./mpv.conf /home/alarm/.config/mpv/
+DISTRO=${1:-arch}
+USER=${2:-alarm}
+
+[ "$DISTRO" = "arch" ] && sudo pacman -S git base-devel xorg xorg-drivers xorg-xinit spectrwm termite ranger mpv nfs-utils xscreensaver unclutter --noconfirm --needed git clone https://aur.archlinux.org/pikaur.git cd pikaur && makepkg -si --noconfirm && pikaur -S xf86-input-joystick --noconfirm
+echo 'if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then' >> /home/$USER/"$(ls -a /home/$USER | grep profile)"
+echo '  exec startx' >> /home/$USER/"$(ls -a /home/$USER | grep profile)"
+echo 'fi' >> /home/$USER/"$(ls -a /home/$USER | grep profile)"
+cp -f ./.xinitrc /home/$USER/
+cp -f ./.spectrwm.conf /home/$USER/
+mkdir -p /home/$USER/.config/mpv
+cp -f ./mpv.conf /home/$USER/.config/mpv/
 sudo echo '10.0.0.47:/mnt/MergerFS /mnt nfs rw' >> /etc/fstab
 sudo mkdir -p /etc/X11/xorg.conf.d
 sudo cp -f ./51-joystick.conf /etc/X11/xorg.conf.d/
