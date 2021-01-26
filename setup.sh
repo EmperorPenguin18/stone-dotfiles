d60e05eaa0dff37dc151c06cdc44828a4622c740
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-01-26 18:36:20 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ DISTRO=${1:-arch}
 USER=${2:-alarm}
 
 [ "$DISTRO" = "arch" ] && sudo pacman -S git base-devel xorg xorg-xinit spectrwm termite ranger mpv nfs-utils xscreensaver unclutter --noconfirm --needed && git clone https://aur.archlinux.org/pikaur.git && cd pikaur && makepkg -si --noconfirm && pikaur -S xf86-input-joystick --noconfirm
-[ "$DISTRO" = "fedora" ] && sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm && sudo dnf install --assumeyes xorg-x11-server-Xorg xorg-x11-xinit spectrwm alacritty ranger mpv nfs-utils xscreensaver joystick
+[ "$DISTRO" = "fedora" ] && sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm && sudo dnf install --assumeyes xorg-x11-server-Xorg xorg-x11-xinit spectrwm alacritty ranger mpv nfs-utils xscreensaver linuxconsoletools
 [ "$DISTRO" = "debian" ] && sudo apt install -y #
 echo 'if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then' >> /home/$USER/"$(ls -a /home/$USER | grep profile)"
 echo '  exec startx' >> /home/$USER/"$(ls -a /home/$USER | grep profile)"
@@ -22,3 +22,4 @@ sudo cp -f ./51-joystick.conf /etc/X11/xorg.conf.d/
 [ "$DISTRO" != "fedora" ] && sudo cp -f ./config.txt /boot/
 sudo mkdir /etc/systemd/system/getty@tty1.service.d
 sudo cp -f ./override.conf /etc/systemd/system/getty@tty1.service.d/
+sudo sed -i "s/USER/$USER/g" /etc/systemd/system/getty@tty1.service.d/override.conf
