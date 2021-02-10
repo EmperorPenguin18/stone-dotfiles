fd8a445e3ad87c7b852c070824a6f389122a7fb3
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-02-09 20:49:11 -0500

    Create arch.sh

---
+++ arch.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+USER=${2:-alarm}
+
+#Install packages
+sudo pacman -S git base-devel xorg xorg-xinit spectrwm rxvt-unicode ranger mpv nfs-utils unclutter --noconfirm --needed
+git clone https://aur.archlinux.org/pikaur.git
+cd pikaur
+makepkg -si --noconfirm
+pikaur -S xf86-input-joystick --noconfirm
+
+#Auto-login as user
+sudo mkdir /etc/systemd/system/getty@tty1.service.d
+sudo cp -f ./override.conf /etc/systemd/system/getty@tty1.service.d/
+sudo sed -i "s/USER/$USER/g" /etc/systemd/system/getty@tty1.service.d/override.conf
+
+#Auto-start X
+echo 'if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then' >> /home/$USER/"$(ls -a /home/$USER | grep profile)"
+echo '  exec startx' >> /home/$USER/"$(ls -a /home/$USER | grep profile)"
+echo 'fi' >> /home/$USER/"$(ls -a /home/$USER | grep profile)"
+
+#Config files
+cp -f ./.xinitrc /home/$USER/
+cp -f ./.spectrwm.conf /home/$USER/
+mkdir -p /home/$USER/.config/mpv
+cp -f ./mpv.conf /home/$USER/.config/mpv/
+cp -f ./input.conf /home/$USER/.config/mpv/
+sudo mkdir -p /etc/X11/xorg.conf.d
+sudo cp -f ./51-joystick.conf /etc/X11/xorg.conf.d/
+sudo cp -f ./config.txt /boot
+git clone https://github.com/muennich/urxvt-perls
+mkdir -p /home/$USER/.urxvt/ext
+cp -f ./urxvt-perls/keyboard-select /home/$USER/.urxvt/ext/
+cp -f ./urxvt-perls/deprecated/clipboard /home/$USER/.urxvt/ext/
+cp -f ./urxvt-perls/deprecated/url-select /home/$USER/.urxvt/ext/
+cp -f ./.Xresources /home/$USER/
+mkdir -p /home/$USER/.config/ranger/plugins
+cp -f ./rc.conf /home/$USER/.config/ranger/
+cp -f ./scope.sh /home/$USER/.config/ranger/
+chmod +x /home/$USER/.config/ranger/scope.sh
+cp -f ./plugin_file_filter.py /home/$USER/.config/ranger/plugins/
+
+#Auto-mount nfs share
+sudo mkdir /mnt/Media
+cat /etc/fstab > ./fstab
+echo '10.0.0.47:/mnt/MergerFS /media nfs rw' >> ./fstab
+sudo cp -f ./fstab /etc/
