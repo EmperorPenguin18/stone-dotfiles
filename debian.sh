4ca8ade60f43d2e6c5df8df010d5aa536ca250d8
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-02-09 20:44:35 -0500

    Update and rename setup.sh to debian.sh

--- debian.sh
+++ debian.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
-DISTRO=${1:-debian}
-USER=${2:-pi}
+USER=${1:-pi}
 
 #Install packages
-[ "$DISTRO" = "arch" ] && sudo pacman -S git base-devel xorg xorg-xinit spectrwm rxvt-unicode ranger mpv nfs-utils unclutter --noconfirm --needed && git clone https://aur.archlinux.org/pikaur.git && cd pikaur && makepkg -si --noconfirm && pikaur -S xf86-input-joystick --noconfirm
-[ "$DISTRO" = "fedora" ] && sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm && sudo dnf install --assumeyes xorg-x11-server-Xorg xorg-x11-drv-fbdev xorg-x11-xinit spectrwm mesa* rxvt-unicode ranger mpv nfs-utils linuxconsoletools
-[ "$DISTRO" = "debian" ] && sudo apt install -y xserver-xorg xinit libgles2-mesa libgles2-mesa-dev xorg-dev spectrwm rxvt-unicode xsel ranger ffmpegthumbnailer mpv nfs-common unclutter xserver-xorg-input-joystick xserver-xorg-input-all xinput
-
-#Auto-login as user
-[ "$DISTO" = "arch" ] && sudo mkdir /etc/systemd/system/getty@tty1.service.d && sudo cp -f ./override.conf /etc/systemd/system/getty@tty1.service.d/ && sudo sed -i "s/USER/$USER/g" /etc/systemd/system/getty@tty1.service.d/override.conf
+sudo apt install -y xserver-xorg xinit libgles2-mesa libgles2-mesa-dev xorg-dev spectrwm rxvt-unicode xsel ranger ffmpegthumbnailer mpv nfs-common unclutter xserver-xorg-input-joystick xserver-xorg-input-all xinput
 
 #Auto-start X
 echo 'if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then' >> /home/$USER/"$(ls -a /home/$USER | grep profile)"
@@ -24,7 +18,7 @@ cp -f ./mpv.conf /home/$USER/.config/mpv/
 cp -f ./input.conf /home/$USER/.config/mpv/
 sudo mkdir -p /etc/X11/xorg.conf.d
 sudo cp -f ./51-joystick.conf /etc/X11/xorg.conf.d/
-[ "$DISTRO" != "fedora" ] && sudo cp -f ./config.txt /boot
+sudo cp -f ./config.txt /boot
 git clone https://github.com/muennich/urxvt-perls
 mkdir -p /home/$USER/.urxvt/ext
 cp -f ./urxvt-perls/keyboard-select /home/$USER/.urxvt/ext/
