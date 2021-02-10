3fe0fba30b103d4838cf5e487c43a8b0858ab08c
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-02-09 21:26:16 -0500

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -3,7 +3,7 @@
 USER=${2:-alarm}
 
 #Install packages
-sudo pacman -S git base-devel xorg xorg-xinit spectrwm rxvt-unicode xsel ranger nfs-utils unclutter --noconfirm --needed
+pacman -S git base-devel xorg xorg-xinit spectrwm rxvt-unicode xsel ranger nfs-utils unclutter pyalpm python-commonmark --noconfirm --needed
 su $USER -c "git clone https://aur.archlinux.org/pikaur.git"
 cd pikaur
 su $USER -c "makepkg --noconfirm"
@@ -11,12 +11,12 @@ pacman -U *.pkg* --noconfirm --needed
 cd ../
 rm -r pikaur
 pikaur -S xf86-input-joystick ffmpeg-git --noconfirm
-sudo pacman -S ffmpegthumbnailer mpv --noconfirm --needed
+pacman -S ffmpegthumbnailer mpv --noconfirm --needed
 
 #Auto-login as user
-sudo mkdir /etc/systemd/system/getty@tty1.service.d
-sudo cp -f ./override.conf /etc/systemd/system/getty@tty1.service.d/
-sudo sed -i "s/USER/$USER/g" /etc/systemd/system/getty@tty1.service.d/override.conf
+mkdir /etc/systemd/system/getty@tty1.service.d
+cp -f ./override.conf /etc/systemd/system/getty@tty1.service.d/
+sed -i "s/USER/$USER/g" /etc/systemd/system/getty@tty1.service.d/override.conf
 
 #Auto-start X
 echo 'if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then' >> /home/$USER/"$(ls -a /home/$USER | grep profile)"
@@ -29,9 +29,9 @@ cp -f ./.spectrwm.conf /home/$USER/
 mkdir -p /home/$USER/.config/mpv
 cp -f ./mpv.conf /home/$USER/.config/mpv/
 cp -f ./input.conf /home/$USER/.config/mpv/
-sudo mkdir -p /etc/X11/xorg.conf.d
-sudo cp -f ./51-joystick.conf /etc/X11/xorg.conf.d/
-sudo cp -f ./config.txt /boot
+mkdir -p /etc/X11/xorg.conf.d
+cp -f ./51-joystick.conf /etc/X11/xorg.conf.d/
+cp -f ./config.txt /boot
 git clone https://github.com/muennich/urxvt-perls
 mkdir -p /home/$USER/.urxvt/ext
 cp -f ./urxvt-perls/keyboard-select /home/$USER/.urxvt/ext/
@@ -45,7 +45,5 @@ chmod +x /home/$USER/.config/ranger/scope.sh
 cp -f ./plugin_file_filter.py /home/$USER/.config/ranger/plugins/
 
 #Auto-mount nfs share
-sudo mkdir /mnt/Media
-cat /etc/fstab > ./fstab
-echo '10.0.0.47:/mnt/MergerFS /media nfs rw' >> ./fstab
-sudo cp -f ./fstab /etc/
+mkdir /mnt/Media
+echo '10.0.0.47:/mnt/MergerFS /media nfs rw' >> /etc/fstab
