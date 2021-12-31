f4588989f091889132ef84669bbe87108ce79f11
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-12-31 17:47:00 -0500

    Create jellyfin.sh

---
+++ jellyfin.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+#Enter these values manually
+URL=""
+KEY=""
+USER=""
+
+FILE="$1"
+LIBRARY=$(pwd | cut -d '/' -f 4)
+if [ "$LIBRARY" = "Movies" ]
+then
+	echo "Duration:$(mediainfo "$FILE" | grep Duration | head -1 | cut -d ':' -f 2)"
+else
+	USERID=$(curl -sX "GET" "http://$URL/Users?api_key=$KEY" | jq ".[] | select(.Name==\"$USER\") | .Id" | cut -d '"' -f 2)
+	LIBRARYID=$(curl -sX "GET" "http://$URL/Items?api_key=$KEY&userID=$USERID" | jq ".Items[] | select(.Name==\"TV Shows\") | .Id" | cut -d '"' -f 2)
+	TITLE=$(pwd | cut -d '/' -f 5 | cut -d '(' -f 1 | awk '{gsub(/^ +| +$/,"")} {print $0}')
+	TITLEID=$(curl -sX "GET" "http://$URL/Items?api_key=$KEY&userID=$USERID&parentId=$LIBRARYID" | jq ".Items[] | select(.Name==\"$TITLE\") | .Id" | cut -d '"' -f 2)
+	SEASON=$(pwd | cut -d '/' -f 6)
+	SEASONID=$(curl -sX "GET" "http://$URL/Shows/$TITLEID/Seasons?api_key=$KEY" | jq ".Items[] | select(.Name==\"$SEASON\") | .Id" | cut -d '"' -f 2)
+	EPISODE=$((10#$(echo $FILE | awk '{split($1, arr, "[E.]"); print arr[2]}')))
+	METADATA=$(curl -sX "GET" "http://$URL/Shows/$TITLEID/Episodes?api_key=$KEY&seasonId=$SEASONID" | jq ".Items[] | select(.IndexNumber==$EPISODE)")
+	echo "Title: $(echo $METADATA | jq '.Name' | cut -d '"' -f 2)"
+	echo "Duration: $(expr $(echo $METADATA | jq '.RunTimeTicks') / 600000000) mins"
+fi
