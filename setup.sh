e39f0f998f728e95c0a90e40e0496fbbf65d502a
EmperorPenguin18 <60635017+EmperorPenguin18@users.noreply.github.com>
2022-11-30 20:02:56 -0500

    Youtube plugin

--- setup.sh
+++ setup.sh
@@ -39,22 +39,23 @@ su $USER -c "git clone https://github.com/EmperorPenguin18/stone-dotfiles $DIR"
 cd $DIR
 
 #Install packages
-pacman -S polkit xorg-xwayland sway ttf-inconsolata alsa-utils --noconfirm --needed
+pacman -S xorg-xwayland sway alsa-utils --noconfirm --needed
 install_aur antimicrox
 
 #Build mpv
 curl -sL http://mirror.archlinuxarm.org/aarch64/alarm/$(curl -sL http://mirror.archlinuxarm.org/aarch64/alarm/ | grep -m 1 "ffmpeg-rpi" | cut -f 2 -d '>' | cut -f 1 -d '<') > ffmpeg-rpi.pkg.tar.xz
 pacman -U ffmpeg-rpi.pkg.tar.xz
-git clone https://github.com/mpv-player/mpv && cd mpv
+su $USER -c "git clone https://github.com/mpv-player/mpv" && cd mpv
 PKG_CONFIG_PATH=/usr/lib/ffmpeg-rpi/pkgconfig/ meson build && ninja -C build
-#cp build/mpv /usr/bin/mpv
-cd ../
+dotfile "./build/mpv" "/usr/bin/" && cd ../
 
 #Auto-login as user
 dotfile "$DIR/override.conf" "/etc/systemd/system/getty@tty1.service.d/"
 sed -i "s/USER/$USER/g" /etc/systemd/system/getty@tty1.service.d/override.conf
 
-#Auto-start X
+#Auto-start Wayland
+usermod -a -G seat $USER
+systemctl enable seatd
 PROFILE="$(ls -a /home/$USER | grep profile)"
 echo 'if [ -z "${DISPLAY}" ] && [ "$(tty)" = "/dev/tty1" ]; then' >> /home/$USER/$PROFILE
 echo '  exec sway' >> /home/$USER/$PROFILE
@@ -71,6 +72,16 @@ su $USER -c "git clone https://github.com/johnodon/Transparent_Cursor_Theme $DIR
 dotfile "$DIR/Transparent_Cursor_Theme/Transparent/cursor.theme" "/usr/share/icons/Transparent/"
 dotfile "$DIR/Transparent_Cursor_Theme/Transparent/cursors/*" "/usr/share/icons/Transparent/cursors/"
 su $USER -c "git clone https://github.com/EmperorPenguin18/mpv-jellyfin /home/pi/.config/mpv"
+
+#Youtube
+su $USER -c "git clone https://github.com/CogentRedTester/mpv-scroll-list"
+dotfile "$DIR/mpv-scroll-list/scroll-list.lua" "/home/$USER/.config/mpv/script-modules/"
+su $USER -c "git clone https://github.com/CogentRedTester/mpv-user-input"
+dotfile "$DIR/mpv-user-input/user-input-module.lua" "/home/$USER/.config/mpv/script-modules/"
+dotfile "$DIR/mpv-user-input/user-input.lua" "/home/$USER/.config/mpv/scripts/"
+su $USER -c "git clone https://github.com/CogentRedTester/mpv-scripts"
+dotfile "$DIR/mpv-scripts/youtube-search.lua" "/home/$USER/.config/mpv/scripts/"
+dotfile "$DIR/youtube.conf" "/home/$USER/.config/mpv/script-opts/"
 
 #Clean up
 chown -R $USER:$USER /home/$USER
