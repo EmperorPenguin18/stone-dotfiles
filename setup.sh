14ef269aef1cb0ffe142fc9e344e6e35cda965f5
EmperorPenguin18 <60635017+EmperorPenguin18@users.noreply.github.com>
2022-12-03 23:54:37 -0500

    Switched to PipeWire

--- setup.sh
+++ setup.sh
@@ -39,12 +39,13 @@ su $USER -c "git clone https://github.com/EmperorPenguin18/stone-dotfiles $DIR"
 cd $DIR
 
 #Install packages
-pacman -S xorg-xwayland sway alsa-utils --noconfirm --needed
+pacman -S xorg-xwayland sway --noconfirm --needed
 install_aur antimicrox
 
 #Build mpv
 curl -sL http://mirror.archlinuxarm.org/aarch64/alarm/$(curl -sL http://mirror.archlinuxarm.org/aarch64/alarm/ | grep -m 1 "ffmpeg-rpi" | cut -f 2 -d '>' | cut -f 1 -d '<') > ffmpeg-rpi.pkg.tar.xz
 pacman -U ffmpeg-rpi.pkg.tar.xz
+pacman -S wireplumber pipewire-jack hicolor-icon-theme luajit wayland-protocols yt-dlp --asdeps --noconfirm --needed
 su $USER -c "git clone https://github.com/mpv-player/mpv" && cd mpv
 PKG_CONFIG_PATH=/usr/lib/ffmpeg-rpi/pkgconfig/ meson build && ninja -C build
 dotfile "./build/mpv" "/usr/bin/" && cd ../
@@ -72,6 +73,10 @@ su $USER -c "git clone https://github.com/johnodon/Transparent_Cursor_Theme $DIR
 dotfile "$DIR/Transparent_Cursor_Theme/Transparent/cursor.theme" "/usr/share/icons/Transparent/"
 dotfile "$DIR/Transparent_Cursor_Theme/Transparent/cursors/*" "/usr/share/icons/Transparent/cursors/"
 su $USER -c "git clone https://github.com/EmperorPenguin18/mpv-jellyfin /home/pi/.config/mpv"
+DEFAULT=$(wpctl status | grep -m 1 'HDMI' | awk '{print $3}' | cut -f 1 -d '.')
+wpctl set-default $DEFAULT
+wpctl set-mute $DEFAULT 0
+wpctl set-volume $DEFAULT 100%
 
 #Youtube
 su $USER -c "git clone https://github.com/CogentRedTester/mpv-scroll-list"
