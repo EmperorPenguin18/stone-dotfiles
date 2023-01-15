7a057ca76da0a4b2f8b6131853305b3e822a6c1e
EmperorPenguin18 <60635017+EmperorPenguin18@users.noreply.github.com>
2023-01-15 01:26:34 -0500

    yet more fixes

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ Turns your Raspberry Pi into a minimal Jellyfin client. Stream video files direc
 ### How to use:
 1. Install [Manjaro ARM Minimal (RPi 4)](https://manjaro.org/download/) to a micro SD card of at least 8GB. Plug that in to a Raspberry Pi 4 with cooling. Plug in a display, keyboard and optionally ethernet. If using Wi-Fi, SSID and password can be entered during setup.
 2. Boot up and complete the setup.
-3. Log in as root. Run the following commands:
+3. Log in as the user you created. Run the following commands:
 ```
-pacman -Syu --noconfirm
-reboot
+sudo pacman -Syu --noconfirm
+sudo reboot
 curl -sL https://raw.github.com/EmperorPenguin18/stone-dotfiles/main/setup.sh | sh
 ```
-This will take a while. Some stuff needs to be compiled.
+Enter your sudo password. This will take a while. Some stuff needs to be compiled.
 
 4. The Jellyfin plugin is already installed, but needs to be configured according to https://github.com/EmperorPenguin18/mpv-jellyfin#configuration
 5. Reboot. You should automatically enter into mpv (video player).
--- setup.sh
+++ setup.sh
@@ -28,30 +28,30 @@ dotfile ()
 #}
 
 #Setup
-if [ "$(id -u)" -ne 0 ]; then
-  echo "This script must be run as root"
-  exit 1
-fi
-USER=$(ls /home)
+#if [ "$(id -u)" -ne 0 ]; then
+#  echo "This script must be run as root"
+#  exit 1
+#fi
+#USER=$(ls /home)
 DIR="/home/$USER/stone-dotfiles"
-pacman -Sy git base-devel meson --noconfirm --needed
-su $USER -c "git clone https://github.com/EmperorPenguin18/stone-dotfiles $DIR"
+sudo pacman -Sy git base-devel meson --noconfirm --needed
+git clone https://github.com/EmperorPenguin18/stone-dotfiles $DIR
 cd $DIR
 
 #Install packages
-pacman -S sway --noconfirm --needed
+sudo pacman -S sway --noconfirm --needed
 
 build_mesa ()
 {
-  pacman -S python-mako llvm wayland-protocols xorg-xrandr --asdeps --noconfirm --needed
-  su $USER -c "git clone -b dev/pi_drm_format https://gitlab.freedesktop.org/EmperorPenguin18/mesa.git"
+  sudo pacman -S python-mako llvm wayland-protocols xorg-xrandr --asdeps --noconfirm --needed
+  git clone -b dev/pi_drm_format https://gitlab.freedesktop.org/EmperorPenguin18/mesa.git
   cd mesa
   meson setup build -D b_ndebug=true -D b_lto=false -D platforms=x11,wayland -D gallium-drivers=swrast,v3d,vc4 -D dri3=enabled -D egl=enabled -D gbm=enabled -D gles1=disabled -D gles2=enabled -D glvnd=true -D glx=dri -D libunwind=enabled -D llvm=enabled -D lmsensors=enabled -D osmesa=true -D shared-glapi=enabled -D microsoft-clc=disabled -D valgrind=disabled -D tools=[] -D zstd=enabled -D video-codecs=vc1dec,h264dec,h264enc,h265dec,h265enc -D buildtype=plain --wrap-mode=nofallback -D prefix=/usr -D sysconfdir=/etc || exit 1
   meson configure --no-pager
   ninja $NINJAFLAGS -C build && \
-  ninja $NINJAFLAGS -C build install && \
-  rm -f /usr/bin/mesa-overlay-control.py && \
-  ln -sf /usr/lib/libGLX_mesa.so.0 /usr/lib/libGLX_indirect.so.0 || \
+  sudo ninja $NINJAFLAGS -C build install && \
+  sudo rm -f /usr/bin/mesa-overlay-control.py && \
+  sudo ln -sf /usr/lib/libGLX_mesa.so.0 /usr/lib/libGLX_indirect.so.0 || \
   return 1
   cd ../ && return 0
 }
@@ -59,11 +59,11 @@ build_mesa || exit 1 #Only needed until 23.0 releases
 
 build_ffmpeg ()
 {
-  pacman -S libepoxy --asdeps --noconfirm --needed
-  su $USER -c "git clone -b dev/5.1.2/rpi_import_1 https://github.com/EmperorPenguin18/rpi-ffmpeg"
+  sudo pacman -S libepoxy --asdeps --noconfirm --needed
+  git clone -b dev/5.1.2/rpi_import_1 https://github.com/EmperorPenguin18/rpi-ffmpeg
   cd rpi-ffmpeg
   CFLAGS="-march=native -mtune=native" CXXFLAGS="-march=native -mtune=native" ./configure --prefix=/usr --disable-debug --disable-muxers --disable-indevs --disable-outdev=fbdev --disable-outdev=oss --disable-doc --disable-bsfs --disable-ffprobe --disable-sdl2 --disable-stripping --disable-thumb --disable-mmal --enable-gnutls --enable-muxer=rawvideo --enable-sand --enable-v4l2-request --enable-libdrm --enable-epoxy --enable-libudev --enable-vout-drm && \
-  make install || \
+  sudo make install || \
   return 1
   cd ../ && return 0
 }
@@ -71,24 +71,24 @@ build_ffmpeg || exit 1 #Needed for the time being
 
 build_mpv ()
 {
-  pacman -S sdl2 wireplumber pipewire-jack hicolor-icon-theme luajit wayland-protocols yt-dlp libass --asdeps --noconfirm --needed
-  su $USER -c "git clone -b pi_h265 https://github.com/EmperorPenguin18/mpv"
+  sudo pacman -S sdl2 wireplumber pipewire-jack hicolor-icon-theme luajit wayland-protocols yt-dlp libass --asdeps --noconfirm --needed
+  git clone -b pi_h265 https://github.com/EmperorPenguin18/mpv
   git config --global --add safe.directory $DIR/mpv
   cd mpv
   meson setup build -Dsdl2=enabled && ninja -C build && \
-  ninja -C build install || \
+  sudo ninja -C build install || \
   return 1
   cd ../ && return 0
 }
 build_mpv || exit 1 #Needed for the time being
 
 #Auto-login as user
-dotfile "$DIR/override.conf" "/etc/systemd/system/getty@tty1.service.d/"
-sed -i "s/USER/$USER/g" /etc/systemd/system/getty@tty1.service.d/override.conf
+sudo cp "$DIR/override.conf" "/etc/systemd/system/getty@tty1.service.d/"
+sudo sed -i "s/USER/$USER/g" /etc/systemd/system/getty@tty1.service.d/override.conf
 
 #Auto-start Wayland
-usermod -a -G seat $USER
-systemctl enable seatd
+sudo usermod -a -G seat $USER
+sudo systemctl enable seatd
 PROFILE="$(ls -a /home/$USER | grep profile)"
 echo 'if [ -z "${DISPLAY}" ] && [ "$(tty)" = "/dev/tty1" ]; then' >> /home/$USER/$PROFILE
 echo '  exec sway' >> /home/$USER/$PROFILE
@@ -104,25 +104,24 @@ dotfile "$DIR/Transparent_Cursor_Theme/Transparent/cursor.theme" "/usr/share/ico
 dotfile "$DIR/Transparent_Cursor_Theme/Transparent/cursors/*" "/usr/share/icons/Transparent/cursors/"
 
 #Init audio
-pipewire &
-pw-record test.wav && \
-pw-play test.wav && \
-DEFAULT=$(wpctl status | grep -m 1 'HDMI' | awk '{print $3}' | cut -f 1 -d '.') && \
-wpctl set-default $DEFAULT && \
-wpctl set-mute $DEFAULT 0 && \
-wpctl set-volume $DEFAULT 100% && \
-rm test.wav || \
-exit 1
+systemctl --user restart pipewire
+while [ -z $DEFAULT ]
+do
+  DEFAULT=$(wpctl status | grep -m 1 'HDMI' | awk '{print $3}' | cut -f 1 -d '.')
+done
+wpctl set-default $DEFAULT
+wpctl set-mute $DEFAULT 0
+wpctl set-volume $DEFAULT 100%
 
 #Youtube
-su $USER -c "git clone https://github.com/CogentRedTester/mpv-scroll-list $DIR/mpv-scroll-list"
+git clone https://github.com/CogentRedTester/mpv-scroll-list $DIR/mpv-scroll-list
 dotfile "$DIR/mpv-scroll-list/scroll-list.lua" "/home/$USER/.config/mpv/script-modules/"
-su $USER -c "git clone https://github.com/CogentRedTester/mpv-user-input $DIR/mpv-user-input"
+git clone https://github.com/CogentRedTester/mpv-user-input $DIR/mpv-user-input
 dotfile "$DIR/mpv-user-input/user-input-module.lua" "/home/$USER/.config/mpv/script-modules/"
 dotfile "$DIR/mpv-user-input/user-input.lua" "/home/$USER/.config/mpv/scripts/"
-su $USER -c "git clone https://github.com/CogentRedTester/mpv-scripts $DIR/mpv-scripts"
+git clone https://github.com/CogentRedTester/mpv-scripts $DIR/mpv-scripts
 dotfile "$DIR/mpv-scripts/youtube-search.lua" "/home/$USER/.config/mpv/scripts/"
-dotfile "$DIR/youtube.conf" "/home/$USER/.config/mpv/script-opts/"
+dotfile "$DIR/youtube_search.conf" "/home/$USER/.config/mpv/script-opts/"
 
 #Clean up
-chown -R $USER:$USER /home/$USER
+#chown -R $USER:$USER /home/$USER
